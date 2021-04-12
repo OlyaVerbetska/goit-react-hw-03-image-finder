97f1317c2a82f77cd17f383722589cbc442a5d2b
Olga Verbetska <verbetska150@gmail.com>
2021-04-12 18:13:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div className="container"></div>;
+  }
+}
+
+export default App;
